52a0667c18f3d5aff8b942a0e8e9bfea5c967c6f
lubelle <belleliullh@gmail.com>
2019-01-13 19:26:25 -0500

    unit test on service

---
+++ src/app/06-services/todos.component.spec.ts
@@ -0,0 +1,25 @@
+import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
+import { from } from 'rxjs';
+
+describe('TodosComponent', () => {
+  let component: TodosComponent;
+  let service: TodoService;
+
+  beforeEach(() => {
+    service = new TodoService(null);
+    component = new TodosComponent(service);
+  });
+
+  it('should set todos property with the items returned from the server', () => {
+    const todos = [1, 2, 3];
+
+    spyOn(service, 'getTodos').and.callFake(() => {
+      return from([ todos]);
+    });
+
+    component.ngOnInit();
+
+    expect(component.todos).toBe(todos);
+  });
+});
